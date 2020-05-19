28e90a329ff3a18ff41e820f0e889de6956d9bf1
Brocard Charly <brocard.charly@etu.unilim.fr>
2020-05-20 01:56:56 +0200

    first front things

--- BreachBetterApp/src/app/app.module.ts
+++ BreachBetterApp/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponentComponent } from './menu-component/menu-component.component';
+import { HomepageComponentComponent } from './homepage-component/homepage-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponentComponent,
+    HomepageComponentComponent
   ],
   imports: [
     BrowserModule,
