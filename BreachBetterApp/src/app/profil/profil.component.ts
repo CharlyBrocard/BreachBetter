f84678625699fff1e93001b888ac3a245f655c2a
Brocard Charly <brocard.charly@etu.unilim.fr>
2020-07-14 13:15:05 +0200

    responsive things

--- BreachBetterApp/src/app/profil/profil.component.ts
+++ BreachBetterApp/src/app/profil/profil.component.ts
@@ -9,60 +9,72 @@ import { HttpClient, HttpRequest, HttpHeaders } from '@angular/common/http';
 })
 export class ProfilComponent implements OnInit {
 
-  
-  // TEST LOL API
-  // champions : any;
-  // users : any;
+  champions : any;
+  users : any;
 
-  // constructor(private profilService: ProfilService) { }
+  constructor(private httpClient: HttpClient) { }
 
 
-  // ngOnInit(){
+  ngOnInit(){
 
+    // const httpOptions = {
+    //   headers: new HttpHeaders({
+    //     'Content-Type':'application/json'
+    //   })
+    // };
 
-  //   this.profilService.getChampions().subscribe((data)=>{
-  //     console.log(data);
-  //     this.champions = data['champions'];
-  //   });
+    // this.profilService.getChampions().subscribe((data)=>{
+    //   console.log(data);
+    //   this.champions = data['champions'];
+    // });
 
 
     // Requete bonne de test pour l'api jsonplaceholder
 
-    // let resp= this.http.get("https://jsonplaceholder.typicode.com/users");
-    // resp.subscribe((data)=>this.users=data);
+     let resp= this.httpClient.get("https://jsonplaceholder.typicode.com/users");
+     resp.subscribe((data)=>this.users=data);
     
-
-    constructor(private http:HttpClient ) { }
-  
-  
-  
-    ngOnInit(){
-      const httpOptions = {
-        header: new HttpHeaders({
-          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
-          "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
-          "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-          "Origin": "https://developer.riotgames.com",
-          "X-Riot-Token": "RGAPI-9cbad70b-4543-4de7-a9ca-0e199873714a"
-        })
-      }
-  
-      const req = this.http.get<any>('https://developer.riotgames.com/apis#champion-v3/GET_getChampionInfo', {})
-        .subscribe(
-          res => {
-            console.log(res);
-          },
-          err => {
-            console.log(err);
-          }
-        );
+    
     }
-      
-  
+
+   
   }
 
 
+// TEST DE REQUETE
   
-  
+//   constructor(
+//     private http:HttpClient ) { }
+
+
+
+//   ngOnInit(){
+//     const httpOptions = {
+//       header: new HttpHeaders({
+//         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+            //  "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+            // "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+            // "Origin": "https://developer.riotgames.com",
+            //  "X-Riot-Token": "RGAPI-f3492a99-6f8c-40d2-8080-a90aaaf8fb00"
+//       })
+//     }
+
+//     const req = this.http.post<any>('https://developer.riotgames.com/apis', {
+//      freeChampionIds : '23' ,
+//      freeChampionIdsForNewPlayers : '18',
+
+//     })
+//       .subscribe(
+//         res => {
+//           console.log(res);
+//         },
+//         err => {
+//           console.log(err);
+//         }
+//       );
+//   }
+    
+
+// }
     
 
--- BreachBetterApp/src/app/services/profil.service.ts
+++ BreachBetterApp/src/app/services/profil.service.ts
@@ -8,11 +8,11 @@ import { HttpClient } from '@angular/common/http';
 
 export class ProfilService {
 
-  // API_KEY = 'RGAPI-9cbad70b-4543-4de7-a9ca-0e199873714a';
+  API_KEY = 'RGAPI-f3492a99-6f8c-40d2-8080-a90aaaf8fb00';
 
-  // public getChampions(){
-  //   return this.http.get(`https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=${this.API_KEY}`);
-  //}
+  public getChampions(){
+    return this.http.get(`https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=${this.API_KEY}`);
+  }
 //   private riotUrl = 'https://br1.api.riotgames.com/lol/static-
 //   data/v3/champions?api_key=myApiKey';
    constructor(private http: HttpClient) { }
